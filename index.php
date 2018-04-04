a84dbda2381ef20e322b2707648057b0be10be0e
Martint033 <martint033@gmail.com>
2018-04-04 10:23:19 +0200

    premiere ligne de code

--- index.php
+++ index.php
@@ -7,6 +7,13 @@
     <title>Document</title>
 </head>
 <body>
-    
+<?php
+if($dossier = opendir('fichiers-externe')){
+    while(false !== ($fichier = readdir($dossier))){
+        echo$fichier
+    }
+}   
+?>
+
 </body>
 </html>
