3ea5b5b811548ad85e04381aabf11e018a2c0961
vitalj <jeremy.vital@gmail.com>
2018-04-04 10:52:10 +0200

    ajout

--- index.php
+++ index.php
@@ -9,35 +9,18 @@
 <body>
 
 <?php
-<<<<<<< HEAD
-$nb-fichiers = 0
+
+$nbf = 0;
 
 if($dossier = opendir('fichiers-externe'))
 {
     while(false !== ($fichier = readdir($dossier))){
         echo$fichier;
-        $nb-fichiers++;
-        echo $nb-fichiers;
+        $nbf++;
     }
+    echo 'nb fichiers'; echo "$nbf";
 }
-=======
 
-if($dossier = opendir('fichiers-externe')){
-    while(false !== ($fichier = readdir($dossier))){
-        if($fichier != '.' && $fichier != '..' && $fichier != 'index.php'){
-            echo $fichier;
-            echo ", ";
-        }
-    }
-}   
-else { echo "dossier introuvable"; }
-
-<<<<<<< HEAD
-else { echo"erreur ouverture dossier"; }
->>>>>>> 4cbd94d994b3c4a9f6c119dfa67eb6d5f5a0c72e
-=======
-closedir($dossier);
->>>>>>> 922fb5b034d277e9bb81b6a52e0c0f2f23ab1976
 ?>
 
 </body>
