0842b4b34d94cf4fb0531aa1de395c27e63c3e27
claireb25 <claire.bourgeois.cb@gmail.com>
2018-04-09 11:01:04 +0200

    php ok chemin pour travailler l'ajax

--- ajax.js
+++ ajax.js
@@ -20,19 +20,19 @@
 // });
 
 
-function exploration(source){
-    console.log(source);
-    method : 'GET'; 
-    xhr.open("GET", "ex1.php?$url=" + source, true);
-    xhr.send(null);
+// function exploration(source){
+//     console.log(source);
+//     method : 'GET'; 
+//     xhr.open("GET", "ex1.php?url=" + source, true);
+//     xhr.send(null);
     
 fetch("ex1.php").then( // on attend d'avoir complètement chargé le fichier, PUIS (then)on effectue la fonction 
 function (response){
     console.log(response);
     return response.json();
-    }
-).then(function(response){
+}).then(function(response){
     for (element in response){
+        console.log(element);
         for (value in response[element]){ 
                 if (element == 'listFiles'){ //si c'est un fichier
                     console.log(element);
@@ -40,23 +40,17 @@ function (response){
                 } 
                 else if (element == 'listDir') { // si c'est un dossier
                     console.log(element);
-                    document.getElementById('listAjax').innerHTML += '<a href="./' + response[element][value] + ' "> '+ response[element][value] + '</a><br>';
+                    document.getElementById('listAjax').innerHTML += '<a href="/' + response[element][value] + ' "> '+ response[element][value] + '</a><br>';
                 }           
         } 
     }
 })
-}
-
-exploration('*');
 
 document.querySelector('body').addEventListener('click', function(event){
         event.preventDefault();
         console.log(event.target.innerText);
-        
-        exploration(event.target.innerText);
+
 });
     
 
-
-/*Fonction ouvrir fichier dans div */
 
--- ex1.php
+++ ex1.php
@@ -1,28 +1,15 @@
 <?php
+$path = $_GET['dir'];
 
+if (isset ($path)){
+    echo $path;
 
+    $folderContent = array("listFiles" =>"", "listDir" =>"");
+    $folderContent["listFiles"] = array_filter(glob($path."/*"), "is_file");
+    $folderContent["listDir"] = array_filter(glob($path."/*"), "is_dir");
 
-// if($dossier = opendir ('fichiers-externe')){
-//     while(false !== ($elements= readdir($dossier))){
-//         if($elements != '.' && $elements != '..' && $elements != 'index.php')
-//         {
-//             echo'<br><a href="fichiers-externe/'.$elements.'">'.$elements.'</a>' ;
-//         }
-//     }
-//     closedir($dossier);
-// } else {
-//     echo 'Dossier introuvable';
-// } 
- 
-// echo json_encode ($dossier);
+}
 
-$url;
+echo json_encode($folderContent);
 
-$folderContent = array("listFiles" , "listDir" =>"");
-$folderContent["listFiles"] = array_filter (glob($url), "is_file");
-$folderContent["listDir"] = array_filter (glob($url), "is_dir");
-echo json_encode ($folderContent);
-
-
-// var_dump($_GET);
 ?>
