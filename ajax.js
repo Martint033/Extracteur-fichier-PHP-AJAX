25475c36917f25dd8689d6d0d13123f4c91fce0d
claireb25 <claire.bourgeois.cb@gmail.com>
2018-04-10 17:58:28 +0200

    finale version

--- ajax.js
+++ ajax.js
@@ -18,10 +18,12 @@ function explorateur(url){
         for (element in response){
             for (value in response[element]){ 
                 if (element == 'listFiles'){ //si c'est un fichier
-                    document.getElementById('listAjax').innerHTML += response[element][value] +'<br>';
+                    document.getElementById('listAjax').innerHTML += `<div class="oneFile"><div class="test"><img data-type="fichier"src="file-icone.png" width="70" height="70" alt="texte si l'image n'est pas affichée">
+                    <p class="displayFichiers"><span> ${response[element][value]} </span><p></div>
+                    </div>`;
                 } 
                 else if (element == 'listDir') { // si c'est un dossier
-                    document.getElementById('listAjax').innerHTML += '<a href="/' + response[element][value] + ' "> '+ response[element][value] + '</a><br>';
+                    document.getElementById('listAjax').innerHTML += '<div class="oneFile"> <a href="' + response[element][value]+'"><img data-type="dossier" class="imgfolder" src="folder-icone.png" width="70" height="70" alt=""><p><span>' + response[element][value] + '</span></p></a><br></div>';
                 }           
             } 
         }
@@ -31,38 +33,49 @@ function explorateur(url){
 explorateur(url[0]);
 
 function creerBouton() {
-            var button = document.createElement("input" ) ;
-            button.type = "button" ;
-            button.setAttribute("value","retour") ;
-            button.setAttribute("id","display") ;
-            document.getElementById('retour').appendChild(button) ;
-    }
+    var button = document.createElement("input" ) ;
+    button.type = "button" ;
+    button.setAttribute("value","retour") ;
+    button.setAttribute("id","display") ;
+    document.getElementById('retour').appendChild(button) ;
+}
 
 
 
 
 document.querySelector('body').addEventListener('click', function(event){
     if (event.target.tagName == "A"){
+        element = event.target;
         event.preventDefault();
         i++;
         url[i] = event.target.innerText+"/*";
         document.getElementById("listAjax").innerHTML = "";
-        explorateur(url[i]);   
-        
+        explorateur(url[i]);       
     }
-
     else if (event.target.tagName == "INPUT") {
         event.preventDefault();
         if (i > 0){
             url.pop();
             i--;
             document.getElementById("listAjax").innerHTML = "";
-            explorateur(url[i]);
-            console.log(url.length);    
+            explorateur(url[i]);  
         }
     }
-    else {
-        event.preventDefault();
+    else if (event.target.tagName == "IMG") {
+        var data = event.target.getAttribute("data-type");
+        if (data == "dossier"){
+            event.preventDefault();
+            element = event.target.parentNode;
+            i++;
+            url[i] = element.getAttribute("href")+"/*";
+            console.log(url[i]);
+            document.getElementById("listAjax").innerHTML = "";
+            explorateur(url[i]);  
+        }
+        else if (data == "fichier"){
+            event.preventDefault();
+        }
+
     }
     if (url.length >1){
            
